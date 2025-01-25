91681becfa78b8fc9d04607c41ca876be36dcbed
Jordan Murphy <jmurphy6895@gmail.com>
2025-01-24 20:34:31 -0700

    chore: forgot to remove _get_doy from jb2008

--- src/jb2008/jb2008.jl
+++ src/jb2008/jb2008.jl
@@ -1006,15 +1006,3 @@ function _jb2008_ΔTc(F10::Number, lst::Number, ϕ_gd::Number, h::Number)
 
     return ΔTc
 end
-
-function _get_doy(jd::Number)
-
-    # Compute the year given the selected Julian Day.
-    year, _, _, = jd_to_date(jd)
-
-    # Compute the day of the year.
-    doy = jd - date_to_jd(year, 1, 1, 0, 0, 0) + 1
-
-    return doy
-
-end
