b8c9ab0d901864181bbed7f46d10d07125dfe201
Jordan Murphy <jmurphy6895@gmail.com>
2025-01-25 23:44:57 -0700

    chore: additional mooncake rule

--- ext/SatelliteToolboxAtmosphericModelsMooncakeExt.jl
+++ ext/SatelliteToolboxAtmosphericModelsMooncakeExt.jl
@@ -17,6 +17,19 @@ function ChainRulesCore.rrule(::typeof(AtmosphericModels._get_doy), jd::Number)
 
 end
 
+function ChainRulesCore.rrule(::typeof(jd_to_date), jd::Number)
+
+    y = jd_to_date(jd)
+
+    function jd_to_date_pullback(Δ)
+        return (NoTangent(), 0.0, 0.0, 0.0, 0.0, 0.0, Δ)
+    end
+
+    return y, jd_to_date_pullback
+
+end
+
 Mooncake.@from_rrule Mooncake.DefaultCtx Tuple{typeof(AtmosphericModels._get_doy), Base.IEEEFloat}
+Mooncake.@from_rrule Mooncake.DefaultCtx Tuple{typeof(jd_to_date), Base.IEEEFloat}
 
 end
