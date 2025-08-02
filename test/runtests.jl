2d63ccec8fa85234665f61c04a0a1eecb572547c
Jordan Murphy <jmurphy6895@gmail.com>
2025-08-02 21:05:36 +0300

    fix: gating on tests in nightly-ci

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,6 @@ using DelimitedFiles
 using SatelliteToolboxAtmosphericModels
 using SatelliteToolboxBase
 
-using DifferentiationInterface
-using FiniteDiff, ForwardDiff, Enzyme, Mooncake, PolyesterForwardDiff, Zygote
-
-using AllocCheck
-using JET
-using Aqua
-
 @testset "Atmospheric Model Jacchia-Roberts 1971" verbose = true begin
     include("./jr1971.jl")
 end
@@ -29,10 +22,45 @@ end
     include("./exponential.jl")
 end
 
-@testset "Differentiation Tests" verbose = true begin
-    include("./differentiablity.jl")
-end
+if isempty(VERSION.prerelease)
+    # Add Mooncake and Enzyme to the project if not the nightly version
+    # Adding them via the Project.toml isn't working because it tries to compile them before reaching the gating
+    using Pkg
+    Pkg.add("DifferentiationInterface")
+    Pkg.add("Enzyme")
+    Pkg.add("FiniteDiff")
+    Pkg.add("ForwardDiff")
+    Pkg.add("Mooncake")
+    Pkg.add("PolyesterForwardDiff")
+    Pkg.add("Zygote")
 
-@testset "Performance Tests" verbose = true begin
-    include("./performance.jl")
+    Pkg.add("JET")
+    Pkg.add("AllocCheck")
+    Pkg.add("Aqua")
+
+    # Test with Mooncake and Enzyme along with the other backends
+    using DifferentiationInterface
+    using Enzyme, FiniteDiff, ForwardDiff, Mooncake, PolyesterForwardDiff, Zygote
+    const _BACKENDS = (
+        ("ForwardDiff", AutoForwardDiff()),
+        ("Enzyme", AutoEnzyme()),
+        ("Mooncake", AutoMooncake(;config=nothing)),
+        ("PolyesterForwardDiff", AutoPolyesterForwardDiff()),
+        ("Zygote", AutoZygote()),
+    )
+
+    using JET
+    using AllocCheck
+    using Aqua
+
+    @testset "Differentiation Tests" verbose = true begin
+        include("./differentiablity.jl")
+    end
+    
+    @testset "Performance Tests" verbose = true begin
+        include("./performance.jl")
+    end
+else
+    @warn "Differentiation backends not guaranteed to work on julia-nightly, skipping tests"
+    @warn "Performance checks not guaranteed to work on julia-nightly, skipping tests"
 end
